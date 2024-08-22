4048d500cb6551ecdc3abcc83abbc46af1780ee6
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-22 15:06:21 +0100

    Refactored Department Controller
    - Added list of expected query parameters
    - Added a check if the received parameters are in the expected list and returns an error response if not

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -8,8 +8,14 @@ use Illuminate\Http\Request;
 
 class DepartmentController extends Controller
 {
+    private const EXPECTED_KEYS = ['department_id'];
+
     public function __invoke(Request $request)
     {
+        if(array_diff(array_keys($request->all()), self::EXPECTED_KEYS)) {
+            return $this->respondError('Invalid parameter(s)');
+        }
+
         $data = $request->validate([
             'department_id' => ['sometimes', 'string', 'regex:/^[0-9]+$/'],
         ]);
@@ -24,7 +30,7 @@ class DepartmentController extends Controller
     private function departments(){
         $departments = Department::all();
 
-        return is_null($departments)
+        return $departments->isEmpty()
             ? $this->respondNotFound("Departments not found")
             : $this->respondWithSuccess(data: DepartmentResource::collection($departments), message: $departments->count(),);
     }
