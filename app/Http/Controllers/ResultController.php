8f09b985496ac5819e9bf7237a2ef01d70353cd4
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-22 15:05:42 +0100

    Refactored Result Controller
    - Added list of expected query parameters
    - Added a check if the received parameters are in the expected list and returns an error response if not

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -13,8 +13,13 @@ class ResultController extends Controller
 {
     private const MAPPING = ['course_registration_id' => 'id'];
 
+    private const EXPECTED_KEYS = ['registration_number', 'department_id', 'course_id', 'session', 'semester', 'level', 'course_registration_id'];
+
     public function __invoke(Request $request)
     {
+        if(array_diff(array_keys($request->all()), self::EXPECTED_KEYS)) {
+            return $this->respondError('Invalid parameter(s)');
+        }
 
         $validated = $request->validate([
             'registration_number' => ['sometimes', 'string', 'regex:/^EBSU\-\d{4}\-\d{4,6}[A-Z]?$/'],
@@ -55,16 +60,16 @@ class ResultController extends Controller
 
         $results = $query->where($filters)->get();
 
-        return is_null($results)
+        return $results->isEmpty()
             ? $this->respondNotFound('Results Not Found')
-            : $this->respondWithSuccess(data: ResultResource::collection($results), message: $results->count(),);
+            : $this->respondWithSuccess(data: ResultResource::collection($results), message: $results->count());
     }
 
     private function results(): JsonResponse
     {
         $results = Result::query()->limit(100)->get();
 
-        return is_null($results)
+        return $results->isEmpty()
             ? $this->respondNotFound('Results Not Found')
             : $this->respondWithSuccess(data: ResultResource::collection($results), message: $results->count());
     }
