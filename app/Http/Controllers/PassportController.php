b7d053c96d0c7536108006755c382d8096ae6f10
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-22 15:05:58 +0100

    Refactored Passport Controller
    - Added list of expected query parameters
    - Added a check if the received parameters are in the expected list and returns an error response if not

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 
 class PassportController extends Controller
 {
+    private const EXPECTED_KEYS = ['registration_number', 'online_id'];
+
     public function __invoke(Request $request)
     {
+        if(array_diff(array_keys($request->all()), self::EXPECTED_KEYS)) {
+            return $this->respondError('Invalid parameter(s)');
+        }
+
         $validated = $request->validate([
             'registration_number' => ['required_without:online_id', 'string', 'regex:/^EBSU\-\d{4}\-\d{4,6}[A-Z]?$/'],
             'online_id' => ['required_without:registration_number', 'string', 'regex:/^[0-9]+$/'],
