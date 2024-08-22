f561f4d54086bc94dd507864e6815cf94c8f52f5
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-22 15:06:37 +0100

    Refactored Course Controller
    - Added list of expected query parameters
    - Added a check if the received parameters are in the expected list and returns an error response if not

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -9,8 +9,14 @@ use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
+    private const EXPECTED_KEYS = ['course_id'];
+
     public function __invoke(Request $request)
     {
+        if(array_diff(array_keys($request->all()), self::EXPECTED_KEYS)) {
+            return $this->respondError('Invalid parameter(s)');
+        }
+
         $data = $request->validate([
             'course_id' => ['sometimes', 'string', 'regex:/^[0-9]+$/'],
         ]);
